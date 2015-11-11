f75da99ce799c27236ef7982de4f3c7029912c4d
Benjamin Dahl <ben.dahl@mail.com>
2015-11-10 20:03:24 -0800

    First Commit

---
+++ CSM20Final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSM20Final
+//
+//  Copyright (c) 2015 CSM20Final. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
