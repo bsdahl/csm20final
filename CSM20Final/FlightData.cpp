f75fca9c342382588adccb13828059b647e89597
reconrey <reconrey@live.com>
2015-12-08 18:16:01 -0800

    Update FlightData.cpp

--- CSM20Final/FlightData.cpp
+++ CSM20Final/FlightData.cpp
@@ -1,21 +1,24 @@
 #include "FlightData.h"
 
 bool FlightData::addPassenger(const PassengerData& pd) {
+	//If flight is full, do not add a passenger and return false
 	if (seatMap.size() >= MAX_PASSENGERS) return false;
 	seatMap.push_back(pd);
 	return true;
 }
 
 bool FlightData::findPassenger(const PassengerData& pd) {
+	//Look through all passengers in seatMap to find a match
 	for (size_t i = 0; i < seatMap.size(); ++i)
 		if (seatMap[i] == pd)
 			return true;
-	return false;
+	return false; //Not found, returns false
 }
 
 PassengerData FlightData::removePassenger(const PassengerData& pd) {
 	size_t i;
 	PassengerData pdRemoved;
+	//Search all passengers on flight till a match is found + remove
 	for (i = 0; i < seatMap.size(); ++i) {
 		if (seatMap[i] == pd) {
 			pdRemoved = seatMap[i];
@@ -23,12 +26,12 @@ PassengerData FlightData::removePassenger(const PassengerData& pd) {
 			break;
 		}
 	}
+	//return the removed passenger
 	return pdRemoved;
 }
 
 std::ostream& operator<<(std::ostream & out, const FlightData& fd) {
-	char prev(' ');
-
+	//Format the output, align values, fill with zeros, format time
 	out << "  " << std::right << std::setw(4) << std::setfill('0')
 		<< fd.flightNumber << "  " << std::setfill(' ')
 		<< std::setw(12) << std::left << fd.fromCity << "  "
@@ -37,7 +40,6 @@ std::ostream& operator<<(std::ostream & out, const FlightData& fd) {
 		<< ':' << std::setw(2) << fd.departTime % 100 << "  "
 		<< std::setw(2) << fd.arriveTime/ 100
 		<< ':' << std::setw(2) << fd.arriveTime % 100
-		<< "  " << std::setw(4) << fd.mileage << '\n';
-	cout.fill(prev);  // to reset setfill()
+		<< "  " << std::setw(4) << fd.mileage << '\n' << std::setfill('0');
 	return out;
 }
